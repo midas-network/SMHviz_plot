3eca6375eb5afdf96925e199a5448f7e1660a76e
LucieContamin <CONTAMIN@pitt.edu>
2023-11-20 14:45:04 -0500

    add functions for data process for combine plot

--- SMHviz_plot/utils_data.py
+++ SMHviz_plot/utils_data.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import pandas as pd
 
 
@@ -130,3 +130,250 @@ def scen_comparison_data(df, max_week, end_method, comparison_reference, model_e
     # Remove NaN value
     df_all = df_all[~df_all["rel_change"].isna()]
     return df_all
+
+
+def flatten_list(clist):
+    """ Flatten a list
+
+    Flatten a complex list
+
+    :parameter clist: complex list of different level (i.e. list of list)
+    :type clist: list
+    :return: a flatten list
+    """
+    flat_list = [item for sublist in clist for item in sublist]
+    return flat_list
+
+
+def sample_df(df, scenario, pathogen, k=1000):
+    """Sample DataFrame per scenario
+
+    For a specific DataFrame, containing trajectories information in the SMH standard format:
+
+    -  Create a "sample_id" variable corresponding to `<model_name>_<output_type_id>_<scenario_id>`
+    -  For each team_model:
+         - create a `"list_sample"`: list of all the possible `"sample_id"` associated with the team_model.
+           For example: `"modelA_1_scenC"`, `"modelA_2_scenC"`, etc.
+         - create a `"`list_weight"` : list of all each weight associated with each `"sample_id"`.
+           Calculated as: 1/total number of trajectories of the location, target, scenario, model_team.
+           For example: 1/100, 1/100, etc.
+    - Concatenate all the list for each team_model together. For example: `list_sample = "modelA_1_scenC",
+      "modelA_2_scenC", …, "modelA_100_scenC", "modelB_1_scenC", "modelB_2_scenC", …,  modelB_90_scenC", etc.` and
+      `list_weight = 1/100, 1/100, …, 1/100, 1/90, 1/90, …, 1/90, etc.`
+    - Transform the list_weight to sum to 1 by dividing by the number of `model_team` and `scenario` for the `location`,
+      `target`. For example (if we have 2 model team for US, Incident Hospitalization, Flu):
+         `list_weight = (1/100)/2, (1/100)/2, …, (1/100)/2, (1/90)/2, (1/90)/2, …, (1/90)/2`.
+         `list_weight = list_weight/number of scenario inputted` (length of the parameter `scenario`)
+    - Sample the `list_sample` k times (by default, 1000) with the associated weight and with replacement,
+      by applying: `sample = np.random.choice(list_sample, p=weight_sample_fin, size=k, replace=True)`
+    - Shuffle the list to avoid having the list ordered by `scenario`, `model`, `trajectories id`.
+    - Select all the individual trajectories data frame input from the complete `"sample"` list. If a sample_id is
+      sampled multiple times, it will be repeated multiple times in the output data frame.
+    - Recode the sample_id to a numeric corresponding to 0 to number of sample - 1 (by default k-1: 999)
+    - Return the output as a data frame with three columns: date, value_<pathogen name>, sample_id
+
+    :parameter df: DataFrame in the SMH standard format containing all the trajectories associated with a specific
+      round and pathogen
+    :type df: pd.DataFrame
+    :parameter scenario: list of scenario to filter the inputted the data frame with. If list is empty, an empty
+      DataFrame will be return
+    :type scenario: list
+    :parameter pathogen: name of the pathogen associated with the data
+    :type pathogen: str
+    :parameter k: number of samples to draw, by default 1000
+    :type k: int
+    :return: A DataFrame with three columns: date, value_<pathogen name>, sample_id
+    """
+    pathogen = pathogen.lower()
+    if len(scenario) > 0:
+        df_scen = df[df["scenario_id"].isin(scenario)].copy()
+        df_scen["sample_id"] = (df_scen["model_name"].astype(str) + "_" + df_scen["type_id"].astype(str) + "_" +
+                                df_scen["scenario_id"].astype(str))
+        df_scen["pathogen"] = pathogen
+        list_sample = list()
+        weight_sample = list()
+        for model in list(df_scen["model_name"].drop_duplicates()):
+            df_model = df_scen[df_scen["model_name"] == model]
+            list_sample.append(list(df_model["sample_id"].unique()))
+            weight_list = [1 / max(df_model.type_id.drop_duplicates())] * int(
+                len(list(df_model["sample_id"].unique())))
+            weight_sample.append(weight_list)
+        list_sample = flatten_list(list_sample)
+        weight_sample_fin = flatten_list(weight_sample) / np.array(len(list(df_scen["model_name"].drop_duplicates())))
+        weight_sample_fin = weight_sample_fin / np.array(len(scenario))
+        all_sample_sel = np.random.choice(list_sample, p=weight_sample_fin, size=k, replace=True)
+        all_sample_sel = list(np.random.permutation(all_sample_sel))
+        df_sample_sel = df_scen.reset_index().set_index("sample_id").loc[all_sample_sel]
+        all_sample = df_sample_sel.reset_index().drop(columns="index")
+        all_sample["sample_id_n"] = list(np.repeat(list(range(k)), len(df_scen['horizon'].drop_duplicates())))
+        all_sample = all_sample[["value", "target_end_date", "sample_id_n"]]
+        all_sample = all_sample.rename(columns={"value": "value" + "_" + pathogen})
+    else:
+        all_sample = pd.DataFrame(columns=["value" + "_" + pathogen, "target_end_date", "sample_id_n"])
+    return all_sample
+
+
+def q1(x):
+    """Calculate the quantile 0.025
+
+    Calculate the quantile 0.025 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.025), 2)
+
+
+def q2(x):
+    """Calculate the quantile 0.05
+
+    Calculate the quantile 0.05 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.05), 2)
+
+
+def q3(x):
+    """Calculate the quantile 0.1
+
+    Calculate the quantile 0.1 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.1), 2)
+
+
+def q4(x):
+    """Calculate the quantile 0.25
+
+    Calculate the quantile 0.25 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.25), 2)
+
+
+def q5(x):
+    """Calculate the quantile 0.75
+
+    Calculate the quantile 0.75 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.75), 2)
+
+
+def q6(x):
+    """Calculate the quantile 0.9
+
+    Calculate the quantile 0.9 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.9), 2)
+
+
+def q7(x):
+    """Calculate the quantile 0.95
+
+    Calculate the quantile 0.95 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.95), 2)
+
+
+def q8(x):
+    """Calculate the quantile 0.975
+
+    Calculate the quantile 0.975 on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.975), 2)
+
+
+def med(x):
+    """Calculate the quantile 0.5 (or median)
+
+    Calculate the quantile 0.5 (or median) on a specific pandas Series and return the round value to 2 digits.
+
+    :parameter x: A pandas Series of value
+    :type x: pd.Series
+    :return: float
+    """
+    return round(x.quantile(0.5), 2)
+
+
+def prep_multipat_plot_comb(pathogen_information):
+    """Process Data for Combined Multi-pathogen plot
+
+    From a dictionary containing each DataFrame associated to a specific pathogen:
+
+    - `"value"`: Sum of all the "value_" columns ("value_<pathogen>" set to NA for pathogen with empty DataFrame
+      (not selected))
+    - Proportion of each pathogen `"proportion_<pathogen>" = "value_<pathogen>" / "value"`
+    - Calculate the median, 95%, 90%, 80%, and 50% quantiles for each "value" and "proportion" columns
+
+    The input `pathogen_information` should be in a specific format:
+
+    - `pathogen_information = {<pathogenA>: {"dataframe":<DataFrame>}, <pathogenB>: {"dataframe":<DataFrame>}, etc.}`
+    - `<DataFrame>` is a data frame in the output format of the `sample_df()` function with 3 columns:
+      "target_end_date", "sample_id_n" and "value_<pathogen>".
+    - For more information, please consult the `sample_df()` function documentation
+
+    :parameter pathogen_information: A dictionary containing multiple dictionary containing a DataFrame (result of
+     sampling process, key: "dataframe") and named with the associated specific pathogen (keys).
+    :type pathogen_information: dict
+    :return: A dictionary with 2 objects: (1) "all":  median, 95%, 90%, 80%, and 50% quantiles for each "value" and
+     "value_<pathogen> "columns and (2) "detail": median, 95%, 90%, 80%, and 50% quantiles for each
+     "proportion_<pathogen>" columns.
+    """
+    all_sample = pd.DataFrame()
+    f = {'value': [med, q1, q2, q3, q4, q5, q6, q7, q8]}
+    f2 = {'proportion': [med, q1, q2, q3, q4, q5, q6, q7, q8]}
+    for patho in pathogen_information:
+        # Preparation
+        pathogen_name = patho.lower()
+        f.update({"value_" + pathogen_name: [med, q1, q2, q3, q4, q5, q6, q7, q8]})
+        # Merge all pathogen in one dataframe
+        if len(all_sample) > 0:
+            if len(pathogen_information[patho]["dataframe"]) > 0:
+                all_sample = pd.merge(all_sample, pathogen_information[patho]["dataframe"],
+                                      on=["target_end_date", "sample_id_n"])
+            else:
+                all_sample = all_sample.copy()
+                all_sample["value_" + pathogen_name] = pd.NA
+        else:
+            all_sample = pathogen_information[patho]["dataframe"]
+    # Calculate sum of all pathogen
+    all_sample["value"] = all_sample[[col for col in all_sample.columns if col.startswith('value_')]].sum(axis=1)
+    # Calculate proportion of each pathogen
+    for patho in pathogen_information:
+        # Preparation
+        pathogen_name = patho.lower()
+        f2.update({"proportion_" + pathogen_name: [med, q1, q2, q3, q4, q5, q6, q7, q8]})
+        all_sample["proportion_" + pathogen_name] = all_sample["value_" + pathogen_name] / all_sample["value"]
+    # Calculate the quantiles for each "value" and "proportion" columns
+    all_quantile = all_sample.groupby(["target_end_date"]).agg(f)
+    all_quantile.columns = all_quantile.columns.get_level_values(0) + "-" + all_quantile.columns.get_level_values(1)
+    detail_quantile = all_sample.groupby(["target_end_date"]).agg(f2)
+    detail_quantile.columns = (detail_quantile.columns.get_level_values(0) + "-" +
+                               detail_quantile.columns.get_level_values(1))
+    return {"all": all_quantile, "detail": detail_quantile}
